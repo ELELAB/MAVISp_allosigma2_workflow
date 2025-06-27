47ee2931b53a7e9299040a679bb9c98977050d41
elenapapaleo <elena.papaleo.78@gmail.com>
2025-06-27 16:46:22 +0200

    Update do.sh

--- simple_mode/2.allosigma_classify/do.sh
+++ simple_mode/2.allosigma_classify/do.sh
@@ -1,2 +1,2 @@
-source /usr/local/envs/py310/bin/activate
+source /usr/local/envs/py310/bin/activate #on our local server only
 ./allosigma-classify $3 $1 aminoacids.dat $2 -c 5 -o allosigma_mut.txt
